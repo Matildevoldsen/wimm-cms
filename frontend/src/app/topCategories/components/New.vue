1d297af11458123879f8c8a90fedd24dc652c639
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2020-01-17 21:04:07 +0000

    Create form field for new top category.

--- frontend/src/app/topCategories/components/New.vue
+++ frontend/src/app/topCategories/components/New.vue
@@ -6,7 +6,39 @@
             </div>
             <div class="column is-10">
                 <div class="box m-t-50">
+                    <section>
+                        <b-field label="Title" :label-position="labelPosition">
+                            <b-input placeholder="Category Title"></b-input>
+                        </b-field>
 
+                        <b-field label="Description"
+                                 :label-position="labelPosition">
+                            <b-input placeholder="Category Description"></b-input>
+                        </b-field>
+
+                        <div class="field">
+                            <b-switch>Keep category private?</b-switch>
+                        </div>
+
+                        <div class="field">
+                            <b-switch>Show the page for this category?</b-switch>
+                        </div>
+
+                        <b-field class="file">
+                            <b-upload v-model="file">
+                                <a class="button is-primary">
+                                    <b-icon icon="upload"></b-icon>
+                                    <span>Upload thumbnail</span>
+                                </a>
+                            </b-upload>
+                            <span class="file-name" v-if="file">
+                                {{ file.name }}
+                            </span>
+                        </b-field>
+                        <b-field>
+                            <b-button type="is-success">Send</b-button>
+                        </b-field>
+                    </section>
                 </div>
             </div>
         </div>
@@ -14,6 +46,11 @@
 </template>
 <script>
     export default {
-
+        data() {
+            return {
+                file: null,
+                labelPosition: 'on-border'
+            }
+        }
     }
 </script>

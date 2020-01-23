094cc6f2ee3076d851d116493f514ee50e6d7daa
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2020-01-23 14:31:33 +0000

    Outline top cat	egory controller functions and finish following functions:
    - List of all top categories.
    - View particular top category.
    - Validate content when creating top category.
    - Add readable dates on top category.

--- backend/app/Http/Controllers/API/TopCategoryController.php
+++ backend/app/Http/Controllers/API/TopCategoryController.php
@@ -4,8 +4,66 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\TopCategory;
 
 class TopCategoryController extends Controller
 {
-    //
+    public function all() {
+        return TopCategory::all();
+    }
+
+    public function view($id) {
+        $category = TopCategory::find($id);
+
+        if ($category) {
+            if ($category->has_page) {
+                return response()->json(['data' => [
+                    'statusCode' => '200',
+                    'category' => $category,
+                    'readable_dates' => [
+                        'updated_at' => $category->updated_at->diffForHumans(),
+                        'created_at' => $category->created_at->diffForHumans(),
+                    ]
+                ]]);
+            } else {
+                return response()->json([
+                    'errors' => [
+                        'root' => 'This top category does not have a visble page'
+                    ]
+                ]);
+            }
+        } else {
+            return response()->json([
+                'errors' => [
+                    'root' => 'This top category does not exist'
+                ]
+            ]);
+        }
+    }
+
+    public function store(Request $request) {
+        $validator = $request->validate([
+            'title' => 'required|min:2',
+            'description' => 'required|min:20',
+            'thumbnail' => 'required|image'
+        ]);
+
+        if ($validator) {
+            //Finish create function
+        } else {
+            return response([
+                'errors' => [
+                    'root' => [$validator]
+                ]
+            ], 422);
+        }
+    }
+
+    public function update(Request $request) {
+
+    }
+
+    public function delete(Request $request) {
+
+    }
 }

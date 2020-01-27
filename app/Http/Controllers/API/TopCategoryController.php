947f43eca636e9e3e9e5d2b522a3641a9c5a7a38
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2020-01-27 18:27:17 +0000

    Implement it for store category.

--- app/Http/Controllers/API/TopCategoryController.php
+++ app/Http/Controllers/API/TopCategoryController.php
@@ -3,63 +3,51 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTopCategoryRequest;
 use Illuminate\Http\Request;
 use App\TopCategory;
-use phpDocumentor\Reflection\Types\Boolean;
 
-class TopCategoryController extends Controller
-{
+class TopCategoryController extends Controller {
     public function all() {
         $categories = TopCategory::all();
-        return response()->json(['data' => [
+        return response()->json( ['data' => [
             'statusCode' => '200',
             'category' => $categories,
-        ]]);
+        ]] );
     }
 
+    public function view( $id ) {
+        $category = TopCategory::find( $id );
 
-
-    public function view($id) {
-        $category = TopCategory::find($id);
-
-        if ($category) {
-            if ($category->has_page) {
-                return response()->json(['data' => [
+        if ( $category ) {
+            if ( $category->has_page ) {
+                return response()->json( ['data' => [
                     'statusCode' => 200,
                     'category' => $category,
                     'readable_dates' => [
                         'updated_at' => $category->updated_at->diffForHumans(),
                         'created_at' => $category->created_at->diffForHumans(),
                     ]
-                ]]);
+                ]] );
             } else {
-                return response()->json([
+                return response()->json( [
                     'errors' => [
                         'root' => 'This top category does not have a visble page'
                     ]
-                ]);
+                ] );
             }
         } else {
-            return response()->json([
+            return response()->json( [
                 'errors' => [
                     'root' => 'This top category does not exist'
                 ]
-            ]);
+            ] );
         }
     }
 
-    public function store(Request $request) {
-        $validator = $request->validate([
-            'title' => 'required|min:2|max:100',
-            'description' => 'required|min:20',
-            'thumbnail' => 'required|image',
-            'has_page' => 'required',
-            'is_private' => 'required',
-            'show_in_navigation' => 'required',
-            'show_in_footer' => 'required'
-        ]);
-
-        if ($validator) {
+    public function store( StoreTopCategoryRequest $request ) {
+        $validated = $request->validated();
+        if ( $validated ) {
             $category = new TopCategory;
             $category->title = $request->title;
             $category->description = $request->description;
@@ -69,38 +57,22 @@ class TopCategoryController extends Controller
             $category->show_in_navigation = $request->show_in_navigation;
 
             //Upload image
-            $image = $request->file('thumbnail');
+            $image = $request->file( 'thumbnail' );
             $destinationPath = 'public/thumbnail/topCategory';
 
-            if ($image->storeAs("$destinationPath", $image)) {
-                $category->thumbnail = basename($image . '.' .$image->getClientOriginalExtension());
-            } else {
-                return response()->json([
-                    'errors' => [
-                        'root' => 'The image cannot be uploaded.'
-                    ]
-                ]);
+            if ( $image->storeAs( "$destinationPath", $image ) ) {
+                $category->thumbnail = basename( $image . '.' .$image->getClientOriginalExtension() );
             }
 
             $category->save();
 
-            return response()->json([
+            return response()->json( [
                 'data' => $category,
-            ], 200);
-        } else {
-            return response([
-                'errors' => [
-                    'root' => [$validator]
-                ]
-            ], 422);
+            ], 200 );
         }
     }
 
-    public function update(Request $request) {
+    public function update () {}
 
-    }
-
-    public function delete(Request $request) {
-
-    }
+    public function delete() {}
 }

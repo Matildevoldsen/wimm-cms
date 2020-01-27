03d93f0955f44c76cf336bab3ae6de8b8302514b
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2020-01-27 23:06:18 +0000

    Update top category

--- app/Http/Controllers/API/TopCategoryController.php
+++ app/Http/Controllers/API/TopCategoryController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTopCategoryRequest;
+use App\Http\Requests\UpdateTopCategoryRequest;
 use Illuminate\Http\Request;
 use App\TopCategory;
+use Illuminate\Support\Facades\Storage;
 
 class TopCategoryController extends Controller {
-    public function all() {
+    public function all(/*$sort = 'DEFAULT', $where = null, $whereOperator = null, $whereValue = null*/) {
         $categories = TopCategory::all();
         return response()->json( ['data' => [
             'statusCode' => '200',
@@ -16,7 +18,7 @@ class TopCategoryController extends Controller {
         ]] );
     }
 
-    public function view( $id ) {
+    public function view($id) {
         $category = TopCategory::find( $id );
 
         if ( $category ) {
@@ -72,7 +74,38 @@ class TopCategoryController extends Controller {
         }
     }
 
-    public function update () {}
+    public function update (UpdateTopCategoryRequest $request, $id) {
+        $validated = $request->validated();
+        if ( $validated ) {
+            $category = TopCategory::find($id);
+            $category->title = $request->title;
+            $category->description = $request->description;
+            $category->has_page = $request->has_page;
+            $category->is_private = $request->is_private;
+            $category->show_in_footer = $request->show_in_footer;
+            $category->show_in_navigation = $request->show_in_navigation;
 
-    public function delete() {}
+            //Upload image
+            if ($request->hasFile('thumbnail')) {
+                $image = $request->file( 'thumbnail' );
+                $destinationPath = 'public/thumbnail/topCategory';
+
+                Storage::delete($destinationPath . $category->thumbnail);
+
+                if ( $image->storeAs( "$destinationPath", $image ) ) {
+                    $category->thumbnail = basename( $image . '.' .$image->getClientOriginalExtension() );
+                }
+            }
+
+            $category->save();
+
+            return response()->json( [
+                'data' => $category,
+            ], 200 );
+        }
+    }
+
+    public function delete($id) {
+        
+    }
 }
